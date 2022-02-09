fe0b30a6b0a13ab5e822c3e85015af1ec7680dcb
skillzl <user@skillzl.me>
2022-02-10 01:14:55 +0200

    👨‍💻 update: SEO tag manager

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { ThemeProvider } from 'theme-ui';
-import theme from 'theme';
-import SEO from 'components/seo';
-import Layout from 'components/layout';
-import Banner from 'sections/banner';
-import Services from 'sections/services';
+import React from "react";
+import { ThemeProvider } from "theme-ui";
+import theme from "theme";
+import SEO from "@americanexpress/react-seo";
+import Layout from "components/layout";
+import Banner from "sections/banner";
+import Services from "sections/services";
 
 export default function IndexPage() {
   return (
@@ -13,6 +13,10 @@ export default function IndexPage() {
         <SEO
           title="chrlwv.tech - a great Discord bot."
           description="A multipurpose discord client written in javascript. Used to be a freelance start-up project but now it's a public client for all around the world users."
+          siteUrl="http://chrlwv.tech"
+          image={{
+            src: "https://i.imgur.com/O3TKDDx.png",
+          }}
         />
         <Banner />
         <Services />
